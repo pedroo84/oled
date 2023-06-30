bd343a7e36a0056e11fc9ffba49f0a9c1b155dda
pedroo84 <92762952+pedroo84@users.noreply.github.com>
2023-06-29 23:22:37 -0300

    Update oledfix2023.sh

--- oledfix2023.sh
+++ oledfix2023.sh
@@ -4,7 +4,7 @@ echo "Installing dependencies"
 opkg update
 opkg install ifstat iputils-ping
 echo "Downloading patch"
-wget https://transfer.sh/91ocioFmgT/oledfixpedro.tar.gz -O oledfixpedro.tar.gz
+wget https://github.com/pedroo84/oled/blob/main/oledfixpedro.tar.gz -O oledfixpedro.tar.gz
 echo "Extracting files"
 tar -xvzf oledfixpedro.tar.gz -C /etc/ssd/
 echo "Done! Loss receive and send are not available with this patch"
